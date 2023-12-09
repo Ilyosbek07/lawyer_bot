dd44e12c8f14d87f60085d17675ad45d2fddc09d
Ilyos Qarshiboev <i.karshiboev@uic.gruop>
2023-12-09 21:49:07 +0500

    feat:new

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -191,7 +191,6 @@ async def checker(call: types.CallbackQuery, state: FSMContext):
     elements = await db.get_elements()
 
     for element in elements:
-        score += element['limit_score']
         limit_require += element['limit_require']
 
     elements = await db.get_elements()
@@ -205,32 +204,10 @@ async def checker(call: types.CallbackQuery, state: FSMContext):
         status *= await subscription.check(user_id=call.from_user.id,
                                            channel=f'{channel}')
     if status:
-        # text = f"""
-        # ❗️<b>Diqqat bilan o’qing!</b>\n\nHuquqiy maslahat olish va tavsiyalar so’rash 100% bepul. Faqat, huquqiy jihatdan yordamga muhtoj insonlarga bu loyihamiz yetib borishi uchun  sizning yordamingiz juda ham zarur!\n\nBot sizga taqdim etgan referral linkni atigi {limit_require} nafar do'stingizga yuboring va bizga murojaat qilish imkoniga ega bo'ling va bepul maslahat olasiz! \n\n<b>🔗 Taklif postini olish tugmani bosing va taklif qilishni boshlang 👇</b>"""
-        # await call.message.answer(text, reply_markup=menu)
+        text = f"""
+        ❗️<b>Diqqat bilan o’qing!</b>\n\nHuquqiy maslahat olish va tavsiyalar so’rash 100% bepul. Faqat, huquqiy jihatdan yordamga muhtoj insonlarga bu loyihamiz yetib borishi uchun  sizning yordamingiz juda ham zarur!\n\nBot sizga taqdim etgan referral linkni atigi {limit_require} nafar do'stingizga yuboring va bizga murojaat qilish imkoniga ega bo'ling va bepul maslahat olasiz! \n\n<b>🔗 Taklif postini olish tugmani bosing va taklif qilishni boshlang 👇</b>"""
+        await call.message.answer(text, reply_markup=menu)
 
-        try:
-            if_old = await db.select_user(telegram_id=call.from_user.id)
-
-            if if_old[5] == 0:
-                args = await db.select_user(telegram_id=call.from_user.id)
-                args = await db.select_user(telegram_id=call.from_user.id)
-                args_user = await db.select_user(telegram_id=int(args[7]))
-                update_score = int(args_user[4]) + score
-                await db.update_user_score(score=update_score, telegram_id=int(args[7]))
-
-                await bot.send_message(chat_id=int(args[7]),
-                                       text=f"👤 Yangi ishtirokchi qo`shildi\n"
-                                            f"🎗 Siz <b>{update_score}-ta</b> foydalanuvchi taklif qildingiz\n"
-                                            f"🗣 Ko`proq do`stlaringizni taklif qiling!")
-                await db.update_user_oldd(oldd=1, telegram_id=call.from_user.id)
-        except Exception as e:
-            print(e, 'check_sub')
-
-        try:
-            await call.message.delete()
-        except Exception as e:
-            pass
     else:
         button = types.InlineKeyboardMarkup(row_width=1, )
 
@@ -276,48 +253,14 @@ async def admin_profile(message: types.Message):
         status *= await subscription.check(user_id=message.from_user.id,
                                            channel=f'{channel}')
     if status:
-        user = await db.select_user(telegram_id=message.from_user.id)
         elements = await db.get_elements()
         limit_require = 0
         admin = ''
         for element in elements:
             limit_require += element['limit_require']
             admin += element['gifts']
-        if user['score'] > limit_require - 1:
-            await message.answer(
-                f"{admin}")
-        else:
-            url_link = f'https://t.me/huquqshunos_uz_bot?start={message.from_user.id}'
-            lessons = await db.select_related_lessons(button_name="Asosiy qism")
-            text = f"""
-            ❗️<b>Diqqat bilan o’qing!</b>\n\nHuquqiy maslahat olish va tavsiyalar so’rash 100% bepul. Faqat, huquqiy jihatdan yordamga muhtoj insonlarga bu loyihamiz yetib borishi uchun  sizning yordamingiz juda ham zarur!\n\nBot sizga taqdim etgan referral linkni atigi {limit_require} nafar do'stingizga yuboring va bizga murojaat qilish imkoniga ega bo'ling va bepul maslahat olasiz! \n\n<b>🔗 Taklif posti orqali taklif qilishni boshlang 👇</b>"""
-            await message.answer(text)
-
-            if lessons:
-                for i in lessons:
-                    if i[2] == 'video':
-                        await message.answer_video(video=f"{i[3]}", caption=f'{i[5]}\n\n{url_link}')
-                    elif i[2] == 'document':
-                        await message.answer_document(
-                            document=f"{i[3]}", caption=f'{i[5]}\n\n{url_link}'
-                        )
-                    elif i[2] == 'audio':
-                        await message.answer_audio(audio=f"{i[3]}", caption=f"{i[5]}\n\n{url_link}")
-                    elif i[2] == 'photo':
-                        await message.answer_photo(photo=f"{i[3]}", caption=f"{i[5]}\n\n{url_link}")
-                    elif i[2] == 'text':
-                        await message.answer(f"{i[5]}\n\n{url_link}")
-            elements = await db.get_elements()
-            limit_require = 0
-            admin = ''
-            for element in elements:
-                limit_require += element['limit_require']
-                admin += element['gifts']
-
-            await message.answer(
-                f"<b>Yuqoridagi postni do'stlaringiz bilan ulashing. 👆</b>\n\n"
-                f"<b>{limit_require} ta</b> do'stingiz sizning taklif havolingiz orqali bot'ga kirib kanallarga a'zo bo'lsa, bot avtomatik tarzda sizga bizga bog’lanish uchun imkon beradi. \n\n<b>Sizning murojaatingiz muhim ❗️\n\n"
-                f"Sizda {user['score']} ball mavjud 🏆</b>")
+        await message.answer(
+            f"{admin}")
 
     else:
         button = types.InlineKeyboardMarkup(row_width=1, )
@@ -436,8 +379,6 @@ async def send_json():
         user_dict = {}
         user_dict['full_name'] = user[1]
         user_dict['username'] = user[2]
-        user_dict['phone'] = user[3]
-        user_dict['score'] = user[4]
         user_dict['tg_id'] = user[6]
         user_list.append(user_dict)
         await asyncio.sleep(0.05)
@@ -452,14 +393,13 @@ async def json_reader(message: types.Message):
     f = open('users.json', 'r')
     data = json.loads(f.read())
     for user in data:
-        # print(user['tg_id'])
         try:
-            user = await db.add_json_file_user(
+            await db.add_json_file_user(
                 telegram_id=user['tg_id'],
                 username=user['username'],
                 full_name=user['full_name'],
-                phone=user['phone'],
-                score=user['score']
+                phone='---',
+                score='---'
             )
         except Exception as e:
             print(e)
